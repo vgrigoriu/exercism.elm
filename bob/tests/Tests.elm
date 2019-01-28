d23522551feeb20b061a82965f9834acfb4b2050
Victor Grigoriu <vgrigoriu@gmail.com>
2019-01-28 21:20:54 +0200

    Bob: solution

--- bob/tests/Tests.elm
+++ bob/tests/Tests.elm
@@ -95,20 +95,17 @@ tests =
                     Expect.equal
                         "Fine. Be that way!"
                         (Bob.hey "")
-        , skip <|
-            test "prolonged silence" <|
+        , test "prolonged silence" <|
                 \() ->
                     Expect.equal
                         "Fine. Be that way!"
                         (Bob.hey "       ")
-        , skip <|
-            test "alternate silences" <|
+        , test "alternate silences" <|
                 \() ->
                     Expect.equal
                         "Fine. Be that way!"
                         (Bob.hey "\t  \n  \t   ")
-        , skip <|
-            test "on multiple line questions" <|
+        , test "on multiple line questions" <|
                 \() ->
                     Expect.equal
                         "Whatever."
