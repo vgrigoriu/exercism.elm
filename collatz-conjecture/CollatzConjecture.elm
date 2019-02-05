21b6a3d99613826fe61d2057b8cfa06a6c499807
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-05 17:47:47 +0200

    Collatz solution

--- collatz-conjecture/CollatzConjecture.elm
+++ collatz-conjecture/CollatzConjecture.elm
@@ -3,4 +3,14 @@ module CollatzConjecture exposing (collatz)
 
 collatz : Int -> Result String Int
 collatz start =
-    Debug.todo "Please implement this function"
+    if start <= 0 then
+        Err "Only positive numbers are allowed"
+
+    else if start == 1 then
+        Ok 0
+
+    else if modBy 2 start == 0 then
+        Result.map ((+) 1) (collatz (start // 2))
+
+    else
+        Result.map ((+) 1) (collatz (3 * start + 1))
