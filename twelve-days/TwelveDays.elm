f8b33356a55af7a2e344eeacff2b02d3cb116c73
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-08 14:18:08 +0200

    Twelve days first test

--- twelve-days/TwelveDays.elm
+++ twelve-days/TwelveDays.elm
@@ -1,6 +1,34 @@
 module TwelveDays exposing (recite)
 
+import Array exposing (..)
+
 
 recite : Int -> Int -> List String
 recite start stop =
-    Debug.todo "Please implement this function"
+    List.range start stop
+        |> List.map verse
+
+
+things =
+    Array.fromList
+        [ "", "a Partridge in a Pear Tree" ]
+
+
+days =
+    Array.fromList [ "", "first" ]
+
+
+verse : Int -> String
+verse number =
+    let
+        day =
+            days
+                |> Array.get number
+                |> Maybe.withDefault "nanana"
+
+        thing =
+            things
+                |> Array.get number
+                |> Maybe.withDefault "yiyiyi"
+    in
+    "On the " ++ day ++ " day of Christmas my true love gave to me, " ++ thing ++ "."
