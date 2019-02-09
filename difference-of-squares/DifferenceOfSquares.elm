1bb86602d59d06cae4a4cc552c247c17ba055b8b
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-09 16:19:07 +0200

    Difference of Squares

---
+++ difference-of-squares/DifferenceOfSquares.elm
@@ -0,0 +1,16 @@
+module DifferenceOfSquares exposing (difference, squareOfSum, sumOfSquares)
+
+
+squareOfSum : Int -> Int
+squareOfSum n =
+    Debug.todo "Please implement this function"
+
+
+sumOfSquares : Int -> Int
+sumOfSquares n =
+    Debug.todo "Please implement this function"
+
+
+difference : Int -> Int
+difference n =
+    Debug.todo "Please implement this function"
