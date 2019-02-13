524632b49b8376da675248820678e6147e066b97
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-13 21:47:23 +0200

    Space Age

---
+++ space-age/SpaceAge.elm
@@ -0,0 +1,17 @@
+module SpaceAge exposing (Planet(..), ageOn)
+
+
+type Planet
+    = Mercury
+    | Venus
+    | Earth
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+
+ageOn : Planet -> Float -> Float
+ageOn planet seconds =
+    Debug.todo "Please implement this function"
