06c106018ca0213c2b0022e4516072493d6b4eef
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-13 21:58:34 +0200

    Space Age solution

--- space-age/SpaceAge.elm
+++ space-age/SpaceAge.elm
@@ -14,4 +14,36 @@ type Planet
 
 ageOn : Planet -> Float -> Float
 ageOn planet seconds =
-    Debug.todo "Please implement this function"
+    seconds / earthYearInSeconds / orbitalPeriodInEarthYears planet
+
+
+orbitalPeriodInEarthYears : Planet -> Float
+orbitalPeriodInEarthYears planet =
+    case planet of
+        Earth ->
+            1
+
+        Mercury ->
+            0.2408467
+
+        Venus ->
+            0.61519726
+
+        Mars ->
+            1.8808158
+
+        Jupiter ->
+            11.862615
+
+        Saturn ->
+            29.447498
+
+        Uranus ->
+            84.016846
+
+        Neptune ->
+            164.79132
+
+
+earthYearInSeconds =
+    31557600
