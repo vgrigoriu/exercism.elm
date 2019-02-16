d02f5ba197b9946fd19de7a2e159061ef46b5f0c
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-16 18:18:56 +0200

    Triangle solution

--- triangle/Triangle.elm
+++ triangle/Triangle.elm
@@ -9,4 +9,37 @@ type Triangle
 
 triangleKind : number -> number -> number -> Result String Triangle
 triangleKind x y z =
-    Debug.todo "Please implement this function"
+    if not <| lengthsArePositive x y z then
+        Err "Invalid lengths"
+
+    else if not <| sidesMeet x y z then
+        Err "Violates inequality"
+
+    else if allAreEqual x y z then
+        Ok Equilateral
+
+    else if twoAreEqual x y z then
+        Ok Isosceles
+
+    else
+        Ok Scalene
+
+
+lengthsArePositive : number -> number -> number -> Bool
+lengthsArePositive x y z =
+    x > 0 && y > 0 && z > 0
+
+
+sidesMeet : number -> number -> number -> Bool
+sidesMeet x y z =
+    x + y > z && x + z > y && y + z > x
+
+
+allAreEqual : number -> number -> number -> Bool
+allAreEqual x y z =
+    x == y && y == z
+
+
+twoAreEqual : number -> number -> number -> Bool
+twoAreEqual x y z =
+    x == y || y == z || x == z
