417354026a1eae49f10f71836ba1d4118610af05
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-26 19:38:43 +0200

    tail recursive

--- accumulate/Accumulate.elm
+++ accumulate/Accumulate.elm
@@ -3,9 +3,14 @@ module Accumulate exposing (accumulate)
 
 accumulate : (a -> b) -> List a -> List b
 accumulate func input =
+    List.reverse <| tailAccumulate func input []
+
+
+tailAccumulate : (a -> b) -> List a -> List b -> List b
+tailAccumulate func input accumulator =
     case input of
         [] ->
-            []
+            accumulator
 
         head :: tail ->
-            func head :: accumulate func tail
+            tailAccumulate func tail (func head :: accumulator)
