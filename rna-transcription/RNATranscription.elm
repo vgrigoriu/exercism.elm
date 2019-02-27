3e6a18f39953337e9ab9f62490066ff482f6116b
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-27 19:04:09 +0200

    Use Dna / Rna types

--- rna-transcription/RNATranscription.elm
+++ rna-transcription/RNATranscription.elm
@@ -1,38 +1,84 @@
 module RNATranscription exposing (toRNA)
 
 
+type Dna
+    = DnaT
+    | DnaA
+    | DnaG
+    | DnaC
+    | InvalidDna Char
+
+
+type Rna
+    = RnaA
+    | RnaU
+    | RnaC
+    | RnaG
+    | InvalidRna Char
+
+
 toRNA : String -> Result Char String
 toRNA =
     String.toList
-        >> List.foldl concat (Ok [])
-        >> Result.map List.reverse
-        >> Result.map String.fromList
+        >> List.map toDna
+        >> List.map complement
+        >> List.map fromRna
+        >> String.fromList
+        >> Ok
 
 
-concat : Char -> Result Char (List Char) -> Result Char (List Char)
-concat nucleotide result =
-    case complement nucleotide of
-        Ok n ->
-            Result.map ((::) n) result
-
-        Err n ->
-            Err n
-
-
-complement : Char -> Result Char Char
-complement nucleotide =
+toDna : Char -> Dna
+toDna nucleotide =
     case nucleotide of
         'T' ->
-            Ok 'A'
-
+            DnaT
+    
         'A' ->
-            Ok 'U'
+            DnaA
 
         'G' ->
-            Ok 'C'
-
+            DnaG
+    
         'C' ->
-            Ok 'G'
+            DnaC
 
         _ ->
-            Err nucleotide
+            InvalidDna nucleotide
+
+
+
+complement : Dna -> Rna
+complement nucleotide =
+    case nucleotide of
+        DnaT ->
+            RnaA
+
+        DnaA ->
+            RnaU
+
+        DnaG ->
+            RnaC
+
+        DnaC ->
+            RnaG
+
+        InvalidDna c ->
+            InvalidRna c
+
+fromRna : Rna -> Char
+fromRna rna =
+    case rna of
+        RnaA ->
+            'A'
+    
+        RnaU ->
+            'U'
+
+        RnaC ->
+            'C'
+
+        RnaG ->
+            'G'
+
+        InvalidRna c ->
+            c
