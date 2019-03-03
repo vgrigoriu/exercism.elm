227660cbebe53480575d967e1f774c556b6b257c
Victor Grigoriu <vgrigoriu@gmail.com>
2019-03-03 07:41:13 +0200

    ETL

---
+++ etl/Etl.elm
@@ -0,0 +1,8 @@
+module Etl exposing (transform)
+
+import Dict exposing (Dict)
+
+
+transform : Dict Int (List String) -> Dict String Int
+transform input =
+    Debug.todo "Please implement this function"
