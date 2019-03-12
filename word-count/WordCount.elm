af3228b776b43c069337c30b9fc2a8331ba907dc
Victor Grigoriu <vgrigoriu@gmail.com>
2019-03-12 17:30:11 +0200

    Word Count

---
+++ word-count/WordCount.elm
@@ -0,0 +1,8 @@
+module WordCount exposing (wordCount)
+
+import Dict exposing (Dict)
+
+
+wordCount : String -> Dict String Int
+wordCount sentence =
+    Debug.todo "Please implement this function"
