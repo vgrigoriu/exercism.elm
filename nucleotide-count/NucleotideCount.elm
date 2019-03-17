f1a0d19752c72c3de569bace10921fa19b566e7a
Victor Grigoriu <vgrigoriu@gmail.com>
2019-03-17 08:15:39 +0200

    Nucleotide count

---
+++ nucleotide-count/NucleotideCount.elm
@@ -0,0 +1,34 @@
+module NucleotideCount exposing (nucleotideCounts)
+
+
+type alias NucleotideCounts =
+    { a : Int
+    , t : Int
+    , c : Int
+    , g : Int
+    }
+
+
+nucleotideCounts : String -> NucleotideCounts
+nucleotideCounts =
+    String.toList
+        >> List.foldr increment { a = 0, t = 0, c = 0, g = 0 }
+
+
+increment : Char -> NucleotideCounts -> NucleotideCounts
+increment nucleotide counts =
+    case nucleotide of
+        'A' ->
+            { counts | a = counts.a + 1 }
+
+        'T' ->
+            { counts | t = counts.t + 1 }
+
+        'C' ->
+            { counts | c = counts.c + 1 }
+
+        'G' ->
+            { counts | g = counts.g + 1 }
+
+        _ ->
+            counts
