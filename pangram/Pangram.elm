79084958d4c179d32c32cd4b19fa6319c190ca58
Victor Grigoriu <vgrigoriu@gmail.com>
2019-03-17 09:47:01 +0200

    Pangram solution

--- pangram/Pangram.elm
+++ pangram/Pangram.elm
@@ -1,6 +1,14 @@
 module Pangram exposing (isPangram)
 
+import Set
+
 
 isPangram : String -> Bool
-isPangram sentence =
-    Debug.todo "Please implement this function"
+isPangram =
+    String.filter Char.isAlpha
+        >> String.map Char.toLower
+        >> String.toList
+        >> Set.fromList
+        >> Set.size
+        -- a bit of a cheat, it only works if the input only has letters of the English alphabet
+        >> (==) 26
