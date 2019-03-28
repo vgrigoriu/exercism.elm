5030be2bad206faeee26f3186c96c1e75f8333ca
Victor Grigoriu <vgrigoriu@gmail.com>
2019-03-28 15:45:40 -0700

    RLE

---
+++ run-length-encoding/RunLengthEncoding.elm
@@ -0,0 +1,11 @@
+module RunLengthEncoding exposing (decode, encode)
+
+
+encode : String -> String
+encode string =
+    Debug.todo "Please implement this function"
+
+
+decode : String -> String
+decode string =
+    Debug.todo "Please implement this function"
