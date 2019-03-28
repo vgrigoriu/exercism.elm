0ce9ac4ecd805b4c9aa267ba68efc42893ddafa3
Victor Grigoriu <vgrigoriu@gmail.com>
2019-03-28 16:38:30 -0700

    Sublist

---
+++ sublist/Sublist.elm
@@ -0,0 +1,13 @@
+module Sublist exposing (ListComparison(..), sublist)
+
+
+type ListComparison
+    = Equal
+    | Superlist
+    | Sublist
+    | Unequal
+
+
+sublist : List a -> List a -> ListComparison
+sublist alist blist =
+    Debug.todo "Please implement this function"
